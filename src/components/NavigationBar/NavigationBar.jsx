f9e3a8c651cf3ce5fc6b82a222e8bebf8f3e6ce5
mhperera <mhperera1995@gmail.com>
2023-11-04 00:53:19 +0530

    use Navlink

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -8,12 +8,12 @@ const NavigationBar = () => {
   return (
     <Navbar expand="md" className="bg-body-tertiary">
       <Container fluid>
-        <Navbar.Brand className='me-5'><Link href="/">Movies</Link></Navbar.Brand>
+        <Navbar.Brand className='me-5'><Link to="/">Movies</Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Link href="/" className='mx-3'>Home</Link>
-            <Link href="/" className='mx-3'>Movies</Link>
+            <NavLink to="/" className='mx-3'>Home</NavLink>
+            <NavLink to="/movies" className='mx-3'>Movies</NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
