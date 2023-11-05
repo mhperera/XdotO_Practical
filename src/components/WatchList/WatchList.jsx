2ecc1d88a6a08cab93ac8fe6e38f9d27528111fd
mhperera <mhperera1995@gmail.com>
2023-11-06 01:08:12 +0530

    rendered watchlist in the component

--- src/components/WatchList/WatchList.jsx
+++ src/components/WatchList/WatchList.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import { useSelector } from 'react-redux';
+import Thumbnail from '../Thumbnail/Thumbnail';
 
 const WatchList = ({showWatchList, onClickWatchList}) => {
+
+  const wishList = useSelector((state)=>state.watchList.shows)
+
+  console.log(wishList);
+
   return (
     <aside>
       <Offcanvas
@@ -16,7 +23,22 @@ const WatchList = ({showWatchList, onClickWatchList}) => {
           <Offcanvas.Title>My Watch list</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          {/*  */}
+          {
+            wishList.length > 0 
+            ? wishList.map((item) => (
+              <Thumbnail
+                key={item.imdbID}
+                imdbID={item.imdbID}
+                title={item.title}
+                release={item.release}
+                type={item.type}
+                poster={item.poster}
+                desc={item.desc}
+                rating={item.rating}
+              />
+            ))
+            : <div className='full-height'>No Data</div>
+          }
         </Offcanvas.Body>
       </Offcanvas>
     </aside>
